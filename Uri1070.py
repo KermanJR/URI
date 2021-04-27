9e102b5c3f0ed9c74c870b3425640a0cda1d3d55
Kerman Junior <63175481+KermanJR@users.noreply.github.com>
2021-04-27 14:11:05 -0400

    Create Uri1070.py

---
+++ Uri1070.py
@@ -0,0 +1,10 @@
+
+
+c = int(input())
+if c//c == 1:
+    print("{}".format(c+1))
+    print("{}".format(c+3))
+    print("{}".format(c+5))
+    print("{}".format(c+7))
+    print("{}".format(c+9))
+    print("{}".format(c+11))
