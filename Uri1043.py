8d03789038014f7c96540aecd35d8d10b42234c9
Kerman Junior <63175481+KermanJR@users.noreply.github.com>
2021-04-27 13:59:12 -0400

    Create Uri1043.py

---
+++ Uri1043.py
@@ -0,0 +1,14 @@
+def main():
+
+    A, B, C = input().split()
+    A = float(A)
+    B = float(B)
+    C = float(C)
+
+    if A+B > C and A+C > B and C+B > A:
+        print("Perimetro = {:.1f}".format(A+B+C))
+     
+    else:
+        print("Area = {:.1f}".format((A+B)*C/2))
+
+main()
