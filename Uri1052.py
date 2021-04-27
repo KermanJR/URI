4eb264d6cd831fe65a1b11973bde01584219592f
Kerman Junior <63175481+KermanJR@users.noreply.github.com>
2021-04-27 14:05:51 -0400

    Create Uri1052.py

---
+++ Uri1052.py
@@ -0,0 +1,47 @@
+def main():
+  
+    v = int(input())
+
+    if v == 1:
+        print("January")
+
+    elif v == 2:
+        print("February")
+
+    elif v == 3:
+        print("March")
+
+    elif v == 4:
+        print("April")
+
+    elif v == 5:
+        print("May")
+
+    elif v == 6:
+        print("June")
+
+    elif v == 7:
+        print("July")
+
+    elif v == 8:
+        print("August")
+
+    elif v == 9:
+        print("September")
+
+    elif v == 10:
+        print("October")
+
+    elif v == 11:
+        print("November")
+
+    elif v == 12:
+        print("December")
+
+
+main()
+
+
+
+
+
