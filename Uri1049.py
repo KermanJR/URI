bb43acf23a6126128d3b102d8351d8599ad9a34a
Kerman Junior <63175481+KermanJR@users.noreply.github.com>
2021-04-27 14:04:13 -0400

    Create Uri1049.py

---
+++ Uri1049.py
@@ -0,0 +1,39 @@
+def main():
+  
+
+    a = input()
+    b = input()
+    c = input()
+
+    if (a == 'vertebrado'):
+        if (b == 'ave'):
+            if (c == 'carnivoro'):
+                print('aguia')
+            else:
+                print('pomba')
+
+    if (a == 'vertebrado'):
+        if (b == 'mamifero'):
+            if (c == 'onivoro'):
+                print('homem')
+            else:
+                print('vaca')
+
+    if (a == 'invertebrado'):
+        if (b == 'inseto'):
+            if (c == 'hematofago'):
+                print('pulga')
+            else:
+                print('lagarta')
+    
+    if (a == 'invertebrado'):
+        if (b == 'anelideo'):
+            if (c == 'hematofago'):
+                print('sanguessuga')
+            else:
+                print('minhoca')
+                
+                
+main()
+
+    
