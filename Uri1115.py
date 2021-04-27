d360b5b606248330db1adeb656ae54700dd37e33
Kerman Junior <63175481+KermanJR@users.noreply.github.com>
2021-04-27 14:22:44 -0400

    Create Uri1115.py

---
+++ Uri1115.py
@@ -0,0 +1,18 @@
+
+
+N = 1
+while N > 0:
+    x, y = input().split()
+    x = int(x)
+    y = int(y)
+    if x > 0 and y > 0:
+        print("primeiro")
+    elif x < 0 and y > 0:
+        print("segundo")
+    elif x < 0 and y < 0:
+        print("terceiro")
+    elif x > 0 and y < 0:
+        print("quarto")
+        N = N + 1
+    elif x == 0 or y == 0:
+        break
